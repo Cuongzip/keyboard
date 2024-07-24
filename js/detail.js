4f5fe2b85ddc31d29e893ea6b84de1364cb8f5f1
cuong <cuongzip2k6@gmail.com>
2024-07-24 15:52:35 +0700

    done ui cart page

--- js/detail.js
+++ js/detail.js
@@ -17,37 +17,6 @@ for (let navItemElement of listNavItemElement) {
   };
 }
 
-////////////////////////
-const inputQuantityElement = document.querySelector(
-  ".top-right-quantity-input"
-);
-
-inputQuantityElement.oninput = (e) => {
-  let value = e.target.value;
-  if (!Number(value[value.length - 1])) {
-    e.target.value = value.slice(0, value.length - 1);
-  } else if (!Number(value[0])) {
-    e.target.value = value.slice(1, value.length);
-  }
-};
-inputQuantityElement.onblur = (e) => {
-  if (!e.target.value) e.target.value = "1";
-};
-const buttonIncrease = document.querySelector(
-  ".top-right-quantity-button-increase"
-);
-const buttonReduce = document.querySelector(
-  ".top-right-quantity-button-reduce"
-);
-buttonIncrease.onclick = () => {
-  const value = Number(inputQuantityElement.value) + 1;
-  if (value <= 999) inputQuantityElement.value = value;
-};
-buttonReduce.onclick = () => {
-  const value = Number(inputQuantityElement.value) - 1;
-  if (value > 0) inputQuantityElement.value = value;
-};
-
 // carousel
 
 let activeIndexImage = 0;
